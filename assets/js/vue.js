1cdd5b2556409b048c8a0677b6d5572e7bf35312
Guy Roger Eboulet <eboulet87@gmail.com>
2020-12-09 11:15:08 +0100

    Vue router

---
+++ assets/js/vue.js
@@ -0,0 +1,32 @@
+ const Home = {
+     template : '<h1>Home</h1>',
+     name: 'Home'
+ }
+
+ const UserSetting = {
+     template : '<h1>UserSetting</h1>',
+     name: 'UserSetting'
+ }
+
+
+ const Whishlist = {
+     template : '<h1>Whishlist</h1>',
+     name: 'Whishlist'
+ }
+
+
+ const ShoppingCart = {
+     template : '<h1>ShoppingCart</h1>',
+     name: 'ShoppingCart'
+ }
+     
+
+
+const vue = new vue(
+
+    router
+
+).$mount('#app');
+
+
+<h1></h1>
